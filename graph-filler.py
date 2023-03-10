62f2868396378ce86575f47b292f10c7c95c23a3
paulmunyao <paulmunyao094@gmail.com>
2023-03-11 01:07:45 +0300

    First commit

--- graph-filler.py
+++ graph-filler.py
@@ -2,10 +2,10 @@ import os
 from random import randint
 
 for i in range(1, 69):
-    for j in range(0, randint(1, 25)):
+    for j in range(0, randint(1, 2)):
         d = str(i) + "days ago"
-        # with open("file.txt","a") as file:
-            # file.write(d)
+        with open("file.txt","a") as file:
+            file.write(d)
         os.system("git add .")
         os.system('git commit --date="'+ d +'" -m"First commit"')
 
