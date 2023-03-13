7dc809858881f28bdecd99ff7852559e35c447d4
paulmunyao <paulmunyao094@gmail.com>
2023-03-13 10:24:03 +0300

    First commit

--- graph-filler.py
+++ graph-filler.py
@@ -1,7 +1,7 @@
 import os
 from random import randint
 
-for i in range(1, 69):
+for i in range(71):
     for j in range(0, randint(1, 2)):
         d = str(i) + "days ago"
         with open("file.txt","a") as file:
